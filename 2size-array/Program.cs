6126d2e71ef13b8b695acc44878e7f729e2b268a
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-12-15 17:27:48 +0400

    test18

---
+++ 2size-array/Program.cs
@@ -0,0 +1,31 @@
+﻿int [,] array =
+{
+    {2,7,9,4,3},
+    {6,3,8,12,2},
+    {7,2,8,0,3}
+    
+};
+
+int height = array.GetLength(0);
+int weight = array.GetLength(1);
+int length = array.Length;
+double sum = 0;
+double average_sum = 0;
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < weight; j++)
+    {
+        Console.Write(array[i,j] + "\t");
+        sum = sum + array[i,j];
+    }
+    Console.WriteLine();
+    
+    
+}
+Console.WriteLine();
+average_sum = sum / length;
+Console.WriteLine(length);
+Console.WriteLine(sum);
+Console.WriteLine(average_sum);
+
